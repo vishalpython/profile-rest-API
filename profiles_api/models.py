4611f170f0af0f871507c815fd5f5cae28683093
vishal <vishalgore889@gmail.com>
2020-10-11 11:47:52 +0530

    Added custom user profile model, manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager
+)
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profile"""
+
+    def create_user(self, email, name, phone, password=None):
+        """Create new user"""
+        if not email:
+            raise ValueError('User must have an email address')
+        if not phone:
+            raise ValueError('User must have  a phone number')
+
+        email = self.normalize_email(email)
+        user = self.models(email=email, name=name, phone=phone)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+def create_superuser(self, email, name, phone, password):
+    """Create and save new super user with given details"""
+    user = self.create_user(email, name, phone, password)
+
+    user.is_superuser = True
+    user.is_staff = True
+    user.save(using=self._db)
+
+    return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database Model for user in system"""
+    email = models.EmailField(max_length=225, unique=True)
+    name = models.CharField(max_length=225)
+    phone = models.IntegerField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'phone']
+
+    def get_full_name(self):
+        """Retrieve full name of user """
+        return self.name
+
+    def get_short_name(self):
+        """Retrieve short name of user"""
+        return self.name
+
+    def __str__(self):
+        """Return string representation of our user"""
+        return self.email
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'profiles_api',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
